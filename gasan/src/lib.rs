169417bf07547a35594813a777457b4dbe9b0c1d
Your Name <you@example.com>
2025-01-31 13:55:08 +0000

    Add gasan

---
+++ gasan/src/lib.rs
@@ -0,0 +1,54 @@
+use {
+    asan::{
+        allocator::{
+            backend::mimalloc::MimallocBackend,
+            frontend::{default::DefaultFrontend, Allocator},
+        },
+        mmap::libc::LibcMmap,
+        shadow::guest::{DefaultShadowLayout, GuestShadow},
+        tracking::guest::GuestTracking,
+        GuestAddr,
+    },
+    std::{
+        ptr::null_mut,
+        sync::{LazyLock, Mutex},
+    },
+};
+
+pub type ZasanAllocator =
+    DefaultFrontend<MimallocBackend, GuestShadow<LibcMmap, DefaultShadowLayout>, GuestTracking>;
+
+static ALLOCATOR: LazyLock<Mutex<ZasanAllocator>> = LazyLock::new(|| {
+    Mutex::new({
+        let backend = MimallocBackend::new();
+        let shadow = GuestShadow::<LibcMmap, DefaultShadowLayout>::new().unwrap();
+        let tracking = GuestTracking::new().unwrap();
+        ZasanAllocator::new(
+            backend,
+            shadow,
+            tracking,
+            ZasanAllocator::DEFAULT_REDZONE_SIZE,
+            ZasanAllocator::DEFAULT_QUARANTINE_SIZE,
+        )
+        .unwrap()
+    })
+});
+
+#[no_mangle]
+pub extern "C" fn zasan_allocate(size: usize, align: usize) -> *mut u8 {
+    if let Ok(mut allocator) = ALLOCATOR.lock() {
+        let addr = allocator.alloc(size, align).unwrap();
+        addr as *mut u8
+    } else {
+        null_mut()
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn zasan_deallocate(addr: *mut u8) -> bool {
+    if let Ok(mut allocator) = ALLOCATOR.lock() {
+        allocator.dealloc(addr as GuestAddr).is_ok()
+    } else {
+        false
+    }
+}
